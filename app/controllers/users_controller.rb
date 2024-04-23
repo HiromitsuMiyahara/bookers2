93491a0aed30bd67ed6b76084f3e613440905ec4
EC2 Default User <164721692+HiromitsuMiyahara@users.noreply.github.com>
2024-04-23 13:47:57 +0000

    [Finish] 4-27

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,6 @@ class UsersController < ApplicationController
     @book = Book.new
   end
 
-  # def create
-  #   @book = Book.new
-  #   @book.user_id = current_user.id
-  #   @book.save
-  #   redirect_to book_path(book.id)
-  # end
-
   def show
     @book = Book.new
     @user = User.find(params[:id])
@@ -44,7 +37,7 @@ class UsersController < ApplicationController
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to book_path
+      redirect_to "/users/#{current_user.id}"
     end
   end
 
