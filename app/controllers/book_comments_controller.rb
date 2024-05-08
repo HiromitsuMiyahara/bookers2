02af094850f3b08e47aba38c987a985e4599d432
EC2 Default User <164721692+HiromitsuMiyahara@users.noreply.github.com>
2024-05-08 10:04:26 +0000

    Finish] 6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path) #redirect_to request.refererでもOK
+    @comment = BookComment.new(book_comment_params)
+    @comment.user_id = current_user.id
+    @comment.book_id = book.id
+    @comment.save
+    # redirect_back(fallback_location: root_path) #redirect_to request.refererでもOK
   end
 
   def destroy
-    comment = BookComment.find(params[:book_id])
-    comment.destroy
-    redirect_back(fallback_location: root_path) #redirect_to request.refererでもOK
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
+    # redirect_back(fallback_location: root_path) #redirect_to request.refererでもOK
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_back(fallback_location: root_path) #redirect_to request.refererでもOK
+    # redirect_back(fallback_location: root_path) #redirect_to request.refererでもOK
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path) #redirect_to request.refererでもOK
+    # redirect_back(fallback_location: root_path) #redirect_to request.refererでもOK
   end
 end
